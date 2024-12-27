eb3f33f7cd0fdfac8dfb7a277dc7aff95643c878
Shashwat <128793378+strange051@users.noreply.github.com>
2024-12-27 17:20:50 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,14 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-
+const firebaseConfig = {
+  apiKey: "AIzaSyA3SakheLagwIDe5peoHOCXllgMDs6aVBk",
+  authDomain: "notes-app-bf5fe.firebaseapp.com",
+  projectId: "notes-app-bf5fe",
+  storageBucket: "notes-app-bf5fe.firebasestorage.app",
+  messagingSenderId: "495187901574",
+  appId: "1:495187901574:web:81c217e9ca5acdbb3fa507"
+};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app); // Initialize and export auth
